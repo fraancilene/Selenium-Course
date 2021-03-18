250066cfd7982d6232d135ead1d8d7d17168d50b
fraancilene <francilenesilva.fps10@gmail.com>
2021-03-18 20:01:17 +0000

    testando sicronismo com Ajax

--- src/main/java/tests/TesteAjax.java
+++ src/main/java/tests/TesteAjax.java
@@ -1,11 +1,15 @@
 package tests;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.DSL;
 
 public class TesteAjax {
@@ -20,7 +24,7 @@ public class TesteAjax {
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\Fran\\Documents\\drivers-navegadores\\chromedriver.exe");
     driverChrome = new ChromeDriver(); // instanciando um objeto chrome
     driverChrome.manage().window().setSize(new Dimension(1200, 765));
-    driverChrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    //driverChrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
     dsl = new DSL(driverChrome);
   }
 
@@ -32,6 +36,12 @@ public class TesteAjax {
 
   @Test
   public  void testAjax(){
-  dsl.escreveNoCampo("j_idt302:name", "Teste");
+    driverChrome.get("https://www.primefaces.org/showcase/ui/ajax/basic.xhtml?jfwid=1cb34");
+    dsl.escreveNoCampo("j_idt302:name", "Teste");
+    dsl.clicarBotao("j_idt302:j_idt306");
+    WebDriverWait wait = new WebDriverWait(driverChrome, 30);
+    //wait.until(ExpectedConditions.textToBe(By.id("j_idt302:display"), "Teste")); // aqui o teste passa quando o texto que foi escrito aparece na tela
+    wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("j_idt345_start"))); // aqui o teste passa quando a imagem de carregamento some da tela
+    Assert.assertEquals("Teste", dsl.obterTexto("j_idt302:display"));
   }
 }
